57bbcf62f59b3b0ac30642074366c8de8caa1b0a
Mallory Hancock <malloryhancock89@gmail.com>
2018-08-28 21:45:28 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+opbot is a cheap alternative to chanserv
+opbot should:
+* when it first joins a channel, it should detect all current ops and add them to the op list
+* automatically give op to anyone on the op list for that channel
+* have a command to add new users to the op list
+* automatically op new user
+* have a command to remove user from op list and should deop said user
---
+++ opbot.py
@@ -0,0 +1,75 @@
+import json
+import time
+
+import irc.bot
+
+irc.client.ServerConnection.buffer_class.errors = 'replace'
+
+class Bot(irc.bot.SingleServerIRCBot):
+    def __init__(self, nickname, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.oplist = self.get_oplist()
+
+    def update_oplist(self, c, channels='all'):
+        if channels == 'all':
+            channels = [i for i in self.oplist]
+        for channel in channels:
+            chan = self.channels[channel]
+            ops = chan.opers()
+            users = chan.users()
+            if channel not in self.oplist:
+                self.oplist[channel] = {'ops': []}
+            for op in ops:
+                if op not in self.oplist[channel]['ops']:
+                    self.oplist[channel]['ops'].append(op)
+            for user in users:
+                if user in self.oplist[channel]['ops'] and user not in ops:
+                    c.mode(channel, '+o {}'.format(user))
+                    time.sleep(1)
+        self.write_oplist()
+
+    def get_oplist(self):
+        with open('oplist.json') as o:
+            oplist = json.load(o)
+        return oplist
+
+    def write_oplist(self):
+        with open('oplist.json', 'w') as o:
+            json.dump(self.oplist, o, indent=2)
+
+    def on_welcome(self, c, e):
+        for channel in self.oplist:
+            c.join(channel)
+
+    def on_pubmsg(self, c, e):
+        text = e.arguments[0]
+        cmd = text.split(' ')[0]
+        if cmd == '!update':
+            self.update_oplist(c, [e.target])
+        elif cmd == '!updateall':
+            self.update_oplist(c)
+
+    def on_invite(self, c, e):
+        c.join(e.arguments[0])
+
+    def on_join(self, c, e):
+        self.update_oplist(c, [e.target])
+
+    def on_mode(self, c, e):
+        if e.source.nick == 'opbot':
+            pass
+        if e.arguments[0] == '-o' and e.arguments[1] in self.oplist[e.target]['ops']:
+            self.oplist[e.target]['ops'].remove(e.arguments[1])
+            self.write_oplist()
+            self.update_oplist(c, [e.target])
+        if e.arguments[0] == '+o':
+            self.update_oplist(c, [e.target])
+
+
+if __name__=='__main__':
+    bot = Bot(
+        nickname='opbot',
+        server='localhost',
+    )
+    bot.start()
+
